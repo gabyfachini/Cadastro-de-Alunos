4c8e69e64e75df80762b281ed128d29a66b9da07
gabyfachini <gabryellafachini@outlook.com>
2024-12-02 11:15:35 -0300

    Update Aluno.cs

--- Cadastro-de-Alunos/Cadastro-de-Alunos/Models/Aluno.cs
+++ Cadastro-de-Alunos/Cadastro-de-Alunos/Models/Aluno.cs
@@ -13,19 +13,22 @@ namespace Cadastro_de_Alunos.Models
             Nome = nome;
             Sobrenome = sobrenome;
         }
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Sobrenome { get; set; }
-        public DateTime Nascimento { get; set; }
-        public char Sexo { get; set; }
-        public string Email { get; set; }
-        public DateTime DataDeCadastro { get; set; }
-        public string Telefone { get; set; }
-        public string Cep { get; set; }
+        public int Id { get; set; } //obrigatório
+        public string Nome { get; set; } //obrigatório
+        public string Sobrenome { get; set; } //obrigatório
+        public DateTime Nascimento { get; set; } //obrigatório
+        public char Sexo { get; set; } //obrigatório
+        public string Email { get; set; } //colocar como obrigatório
+        public DateTime DataDeCadastro { get; set; } //obrigatório
+        public string Telefone { get; set; } //colocar como obrigatório e posteriormente fazer um tratamento para ele ser identificado como Br ou demais países
+        public string Cep { get; set; } //colocar como obrigatório
         public string Logradouro { get; set; }
         public string Complemento { get; set; }
         public string Bairro { get; set; }
         public string Localidade { get; set; }
         public string UF { get; set; }
+        public DateTime? DataDeAtualizacao { get; set; }     //CAMPO DE AUDITORIA - Data da última atualização (pode ser nula) 
+        public bool Ativo { get; set; }                       //CAMPO DE AUDITORIA - 1 = ativo e 0 = excluído - campo obrigatório
+
     }
 }
